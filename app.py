f744bc1a68f1b27147d0d0e18a80f20f98d02601
unknown <jcordero847@unab.edu.co>
2023-06-01 18:15:26 -0500

    DOMINGO3

--- app.py
+++ app.py
@@ -20,14 +20,71 @@ def home():
     if request.method == 'POST':
         text = request.form['text']
         # Aquí es donde procesarías el texto. Por ahora, solo devolvemos el mismo texto. 
-        source_language = ''
+        source_language = '/detect'
 
         translated_text = text
 
         return render_template('home.html', translated_text=translated_text,lang_detected=source_language)
     
     return render_template('home.html')
+# Use the Translator detect function
+path = '/detect'
+url = translator_endpoint + path
 
+# Build the request
+params = {
+    'api-version': '3.0'
+}
+
+headers = {
+'Ocp-Apim-Subscription-Key': cog_key,
+'Ocp-Apim-Subscription-Region': cog_region,
+'Content-type': 'application/json'
+}
+
+body = [{
+    'text': text
+}]
+
+# Send the request and get response
+request = requests.post(url, params=params, headers=headers, json=body)
+response = request.json()
+
+# Parse JSON array and get language
+language = response[0]["language"]
+
+
+
+
+# Use the Translator translate function
+path = '/translate'
+url = translator_endpoint + path
+
+# Build the request
+params = {
+    'api-version': '3.0',
+    'from': source_language,
+    'to': ['en']
+}
+
+headers = {
+    'Ocp-Apim-Subscription-Key': cog_key,
+    'Ocp-Apim-Subscription-Region': cog_region,
+    'Content-type': 'application/json'
+}
+
+body = [{
+    'text': text
+}]
+
+# Send the request and get response
+request = requests.post(url, params=params, headers=headers, json=body)
+response = request.json()
+
+# Parse JSON array and get translation
+translation = response[0]["translations"][0]["text"]
+
+ 
 if __name__ == "__main__":
     app.run(debug=True)
 
