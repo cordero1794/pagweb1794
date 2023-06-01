dd9a7133641fb298f0c82023c190ad8b3a491d2c
unknown <jcordero847@unab.edu.co>
2023-06-01 17:58:04 -0500

    DOMINGO2

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ global cog_key
 global cog_region
 
 try:
-    cog_key = os.environ.get("SECRET_KEY")
-    cog_region = os.environ.get("SECRET_REGION")      
+    cog_key = os.environ.get("COG_SERVICE_KEY")
+    cog_region = os.environ.get("COG_SERVICE_REGION")      
     translator_endpoint = 'https://api.cognitive.microsofttranslator.com'   
 except Exception as ex:        
     print(ex)
@@ -20,69 +20,13 @@ def home():
     if request.method == 'POST':
         text = request.form['text']
         # Aquí es donde procesarías el texto. Por ahora, solo devolvemos el mismo texto. 
-        source_language = '/detect'
+        source_language = ''
 
-        translated_text = '/translate'
+        translated_text = text
 
         return render_template('home.html', translated_text=translated_text,lang_detected=source_language)
     
     return render_template('home.html')
-
-
-# Use the Translator translate function
-path = '/translate'
-url = translator_endpoint + path
-
-# Build the request
-params = {
-    'api-version': '3.0',
-    'from': source_language,
-    'to': ['en']
-}
-
-headers = {
-    'Ocp-Apim-Subscription-Key': cog_key,
-    'Ocp-Apim-Subscription-Region': cog_region,
-    'Content-type': 'application/json'
-}
-
-body = [{
-    'text': text
-}]
-
-# Send the request and get response
-request = requests.post(url, params=params, headers=headers, json=body)
-response = request.json()
-
-# Parse JSON array and get translation
-translation = response[0]["translations"][0]["text"]
-
-
-# Use the Translator detect function
-path = '/detect'
-url = translator_endpoint + path
-
-# Build the request
-params = {
-    'api-version': '3.0'
-}
-
-headers = {
-'Ocp-Apim-Subscription-Key': cog_key,
-'Ocp-Apim-Subscription-Region': cog_region,
-'Content-type': 'application/json'
-}
-
-body = [{
-    'text': text
-}]
-
-# Send the request and get response
-request = requests.post(url, params=params, headers=headers, json=body)
-response = request.json()
-
-# Parse JSON array and get language
-language = response[0]["language"]
 
 if __name__ == "__main__":
     app.run(debug=True)
