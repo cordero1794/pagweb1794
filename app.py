d3239eb3911b7bfb30f517994459ca879a99501a
pagweb1794 <jcordero847@unab.edu.co>
2023-06-02 09:09:24 -0500

    se agrega9

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def home():
 
         languages = ['Spanish', 'English', 'French', 'German']  # Lista de nombres de idiomas
 
-        return render_template('home.html', translations, source_language, languages)
+        return render_template('home.html', translated_text=translations,lang_detected=source_language,languages=languages)
     return render_template('home.html')
 
 def translate_text(text, source_language):
