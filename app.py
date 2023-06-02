fe3e53654917efa0dadfa8f8d437d67fab5c5f1c
pagweb1794 <jcordero847@unab.edu.co>
2023-06-01 19:55:34 -0500

    noche5

--- app.py
+++ app.py
@@ -22,11 +22,39 @@ def home():
         # Aquí es donde procesarías el texto. Por ahora, solo devolvemos el mismo texto. 
         source_language = ''
 
-        translated_text = text
+        translated_text = translate_text(text, source_language)
 
         return render_template('home.html', translated_text=translated_text,lang_detected=source_language)
     
     return render_template('home.html')
+# Use the Translator translate function
+def translate_text(text, source_language):
+    path = '/translate'
+    url = translator_endpoint + path
+
+    # Build the request
+    params = {
+        'api-version': '3.0',
+        'from': source_language,
+        'to': ['en']
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': cog_key,
+        'Ocp-Apim-Subscription-Region': cog_region,
+        'Content-type': 'application/json'
+    }
+
+    body = [{
+        'text': text
+    }]
+
+    # Send the request and get response
+    request = requests.post(url, params=params, headers=headers, json=body)
+    response = request.json()
+
+    # Parse JSON array and get translation
+    translation = response[0]["translations"][0]["text"]
 
 if __name__ == "__main__":
     app.run(debug=True)
