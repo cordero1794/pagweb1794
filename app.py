c566a454bc141314d4fdf6e892d288e00ef64c0b
pagweb1794 <jcordero847@unab.edu.co>
2023-06-01 19:40:06 -0500

    noche3

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ global cog_key
 global cog_region
 
 try:
-    cog_key = os.environ.get("SECRET_KEY")
-    cog_region = os.environ.get("SECRET_REGION")      
+    cog_key = os.environ.get("COG_SERVICE_KEY")
+    cog_region = os.environ.get("COG_SERVICE_REGION")      
     translator_endpoint = 'https://api.cognitive.microsofttranslator.com'   
 except Exception as ex:        
     print(ex)
@@ -28,66 +28,10 @@ def home():
     
     return render_template('home.html')
 
-# Use the Translator detect function
-path = '/detect'
-url = translator_endpoint + path
-
-# Build the request
-params = {
-    'api-version': '3.0'
-}
-
-headers = {
-'Ocp-Apim-Subscription-Key': cog_key,
-'Ocp-Apim-Subscription-Region': cog_region,
-'Content-type': 'application/json'
-}
-
-body = [{
-    'text': text
-}]
-
-# Send the request and get response
-request = requests.post(url, params=params, headers=headers, json=body)
-response = request.json()
-
-# Parse JSON array and get language
-language = response[0]["language"]
-
-
-
-
-# Use the Translator translate function
-path = '/translate'
-url = translator_endpoint + path
-
-# Build the request
-params = {
-    'api-version': '3.0',
-    'from': source_language,
-    'to': ['en']
-}
-
-headers = {
-    'Ocp-Apim-Subscription-Key': cog_key,
-    'Ocp-Apim-Subscription-Region': cog_region,
-    'Content-type': 'application/json'
-}
-
-body = [{
-    'text': text
-}]
-
-# Send the request and get response
-request = requests.post(url, params=params, headers=headers, json=body)
-response = request.json()
-
-# Parse JSON array and get translation
-translation = response[0]["translations"][0]["text"]
-
- 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
 
 
 
